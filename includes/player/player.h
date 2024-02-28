5c0d6fb114d91e21d32b71d522a84e0d1a91515c
foreman, gabriel <gabe.foreman2143@gmail.com>
2024-02-27 16:18:55 -0800

    created and finished the player class

--- includes/player/player.h
+++ includes/player/player.h
@@ -1,5 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <iostream>
 
+using std::cin, std::cout, std::vector, std::string;
+
+class Player{
+    private:
+        char _symbol;
+    public:
+        Player();
+        Player(char s);
+        void set_symbol(char s);
+        char get_symbol();
+};
 
 #endif /* PLAYER_H */
