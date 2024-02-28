96b2b6e03f71fbd444848802f4ae61a604b111a1
carrasco, ryan <rcarrasco5@go.pasadena.edu>
2024-02-28 01:13:35 -0800

    added board class pt3

--- includes/board/board.cpp
+++ includes/board/board.cpp
@@ -1,1 +1,47 @@
 #include "board.h"
+
+Board::Board()
+{
+    rows = 6;
+    columns = 7;
+    board = vector< vector <char> >(6, vector<char>(7, ' '));
+}
+
+Board::Board(int r, int c) 
+{
+    rows = r;
+    columns = c;
+    board = vector< vector<char> >(rows, vector<char>(columns, ' '));
+}
+
+vector < vector<char> > Board::getBoard() const
+{
+    return board;
+}
+
+
+void Board::printBoard() const
+{
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << " | ";
+            cout << board[i][j];
+        }
+        cout << " | ";
+        cout << endl;
+    }
+}
+
+void Board::addSymbolToBoard(int column, char sym)
+{
+    for (int i = rows - 1; i >= 0; i--)
+    {
+        if (board[i][column] == ' ')
+        {
+            board[i][column] = sym;
+            return;
+        }
+    }
+}
--- includes/board/board.h
+++ includes/board/board.h
@@ -1,5 +1,21 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include <iostream>
+#include <vector>
+using std::vector, std::cout, std::endl;
 
+class Board {
+    public:
+        Board();
+        Board(int r, int c);
+        vector < vector<char> > getBoard() const;
+        void printBoard() const;
+        void addSymbolToBoard(int column, char sym);
+        
+    private:
+        vector< vector<char> > board;
+        int rows;
+        int columns;
+};
 
 #endif /* BOARD_H */
