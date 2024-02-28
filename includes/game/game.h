1cdb678773b0404e60d4574b2becc207864d9591
Haurse <mocmatthew5@gmail.com>
2024-02-28 01:22:20 -0800

    did some minor changes in game

--- includes/game/game.h
+++ includes/game/game.h
@@ -1,23 +1,26 @@
 #ifndef GAME_H
 #define GAME_H
 #include <iostream>
+#include "../player/player.h"
 
 using std::cin, std::cout, std::vector, std::string;
 
 class Game{
 private:
-    int x, y;
+    Player x, y;
+    //board
 public:
     Game();
-    Game(int x, int y);
+    Game(Player x, Player y);
 
-    void setX(int x);
-    void setY(int y);
+    void setX(Player x);
+    void setY(Player y);
     void getX();
     void getY();
 
-    void check_move(int x, int y);
-    void check_lrDiagonal(int x, int y);
+    //bool checkMove(Player x, Player y);
+    void checkLrDiagonal(Player x, Player y);
+    //void playerMove();
 };
 
 #endif /* GAME_H */
